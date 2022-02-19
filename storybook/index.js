81f12c78c04fb7d3222aaef984b56a94f19c0dc3
Daniel <daniel.williams@coverflex.com>
2022-02-19 20:58:02 +0000

    fix: add patch

--- storybook/index.js
+++ storybook/index.js
@@ -1,9 +1,7 @@
 // if you use expo remove this line
-import {AppRegistry} from 'react-native';
 
 import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
 import {withKnobs} from '@storybook/addon-knobs';
-import { name as appName } from '../app.json';
 
 import './rn-addons';
 
@@ -15,12 +13,12 @@ configure(() => {
   require('./stories');
 }, module);
 
-// Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
-// To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent(appName, () => StorybookUIRoot);
+const StorybookUIRoot = () =>
+  getStorybookUI({
+    onDeviceUI: true,
+    disableWebsockets: true,
+    // I would recommend actually adding async storage since it adds some nice functionality
+    asyncStorage: null,
+  });
 
 export default StorybookUIRoot;
